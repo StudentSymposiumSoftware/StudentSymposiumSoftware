d3717239a60ba95e2d53ab5b5afe5f2ee71f3797
Sophie Walden <sophie.walden@maine.edu>
2025-02-28 08:29:42 -0500

    Removed unneeded use state

--- SymposiumRecap/src/App.jsx
+++ SymposiumRecap/src/App.jsx
@@ -1,5 +1,4 @@
 import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { useState } from 'react'
 import './App.css'
 
 import HomepageComponent from './components/Homepage';
