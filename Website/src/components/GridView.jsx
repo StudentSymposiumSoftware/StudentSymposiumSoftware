bad3e4505e68d2cc6d196f1b0c605690e0e3573f
Nicholas Pease <me@nicholaspease.com>
2025-03-04 00:04:28 -0500

    Restructure Website

---
+++ Website/src/components/GridView.jsx
@@ -0,0 +1,15 @@
+import "./GridView.css"
+
+function GridView(props) {
+    
+    console.log(props.data) // <-- Here's the data for your ticket
+
+    return (
+        <div id="grid-container">
+            Welcome to the grid view page
+        </div>
+    )
+}
+
+export default GridView
+  
---
+++ Website/src/components/SearchView.jsx
@@ -0,0 +1,15 @@
+import "./SearchView.css"
+
+function SearchView(props) {
+
+    console.log(props.data) // <-- Here's the data for your ticket
+
+    return (
+        <div id="search-container">
+            Welcome to the search page
+        </div>
+    )
+}
+
+export default SearchView
+  
