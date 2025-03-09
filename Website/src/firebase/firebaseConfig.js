18fe9b1691f0f293a6bdfaf3d6f73c5c6f5de815
Sophie Walden <seamus.walden@maine.edu>
2025-03-09 19:11:15 -0400

    feat: connected firebase storage to app

---
+++ Website/src/firebase/firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: "symposiumrecap.firebasestorage.app",
+  messagingSenderId: import.meta.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.REACT_APP_FIREBASE_APP_ID
+}
+
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
+
