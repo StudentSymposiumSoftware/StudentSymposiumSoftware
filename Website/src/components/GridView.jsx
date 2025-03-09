ee014682578a6d18eba3dd90eed073932ef37b1e
ryanshanz <92600751+ryanshanz@users.noreply.github.com>
2025-03-09 15:09:45 -0400

    feat: compact grid view of abstracts

--- Website/src/components/GridView.jsx
+++ Website/src/components/GridView.jsx
@@ -4,12 +4,22 @@ function GridView(props) {
     
     console.log(props.data) // <-- Here's the data for your ticket
 
+
+    
     return (
-        <div id="grid-container">
-            Welcome to the grid view page
+        <div class="grid-container">
+            {props.data.map((item, index) => (
+                <div key={index} className="item" >
+                    {/* Render the item data here */}
+                    <h4>{[item.title, 'by', item.author].join(' ')}</h4>
+                    <p>{[item.major, 'at', item.school].join(' ')}</p>
+                    <p>{['Abstract number', item.abstractNumber].join(' ')}</p>
+                </div>
+      ))}
         </div>
-    )
+    );
+
+
 }
 
 export default GridView
-  
