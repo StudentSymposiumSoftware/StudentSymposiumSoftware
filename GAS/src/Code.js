72d499e859987f33b411e9c31b1009b81b20204b
Nicholas Pease <me@nicholaspease.com>
2025-03-09 23:25:04 +0000

    fix: remove secondary clearScores function
    Issue-Ref: #118

--- GAS/src/Code.js
+++ GAS/src/Code.js
@@ -27,20 +27,6 @@ function showSidebar() {
   SpreadsheetApp.getUi().showSidebar(html);
 }
 
-
-function clearScores() { 
-  const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-  
-  const destinationSheet = spreadsheet.getSheetByName("Student Scores"); 
-  
-  const lastRow = destinationSheet.getLastRow();
-  
-  if (lastRow > 1) {
-    destinationSheet.getRange("2:" + lastRow).clearContent(); 
-  }
-
-}
-
 function assignJudges() {
   // get all necessary data
   const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
