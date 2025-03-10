b8a2e200c9beb99e8f00c9e8182a08ec72fa8f19
Coale1 <coleadams576@gmail.com>
2025-03-09 22:32:12 -0400

    feat: dropdown selectors testing

--- Website/tests/components/SearchView/SearchView.test.tsx
+++ Website/tests/components/SearchView/SearchView.test.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import { expect, it } from "vitest";
 
 import SearchView from '../../../src/components/SearchView';
-import mockData from '../../../src/mockData'
+import mockData from '../../../src/mockData';
 
 it('SearchView renders correctly', () => {
-    const tree = render(<SearchView data={mockData}/>)
-    expect(tree).toMatchSnapshot();
+    const { getByLabelText, getByText, container } = render(<SearchView data={mockData} />);
+
+    expect(container).toMatchSnapshot();
+
+    const majorSelect = getByLabelText(/major/i);
+    fireEvent.change(majorSelect, { target: { value: "" } });
+
+    const professorSelect = getByLabelText(/professor/i);
+    fireEvent.change(professorSelect, { target: { value: "" } });
+
+    const categorySelect = getByLabelText(/category/i);
+    fireEvent.change(categorySelect, { target: { value: "" } });
+
+    const universitySelect = getByLabelText(/university/i);
+    fireEvent.change(universitySelect, { target: { value: "" } });
+
+    expect(container).toMatchSnapshot();
 });
