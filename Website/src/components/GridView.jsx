b5ae2c685d18052d6c3a4bf17849b09f144c61ad
ryanshanz <92600751+ryanshanz@users.noreply.github.com>
2025-03-16 13:37:33 -0400

    feat: set up react router home page / abstract page

--- Website/src/components/GridView.jsx
+++ Website/src/components/GridView.jsx
@@ -3,16 +3,15 @@ import { useNavigate } from "react-router-dom"
 
 function GridView(props) {
     const navigate = useNavigate();
-    
+
     return (
         <div className="grid-container">
             {props.data.map((item, index) => (
                 <div key={index} className="item" >
-  
-                    <h4>{item.title}</h4>
+                    <h4 onClick={() => navigate(`/abstract/${item.abstractNumber}`)}>{item.title}</h4>
                     <h4 onClick={() => navigate(`/author/${item.author}`)} className="author-link">By {item.author}</h4>
-                    <p>{[item.major, 'at', item.school].join(' ')}</p>
-                    <p>{['Abstract number', item.abstractNumber].join(' ')}</p>
+                    <p onClick={() => navigate(`/abstract/${item.abstractNumber}`)}>{[item.major, 'from', item.school].join(' ')}</p>
+                    <p onClick={() => navigate(`/abstract/${item.abstractNumber}`)}>{['Abstract number', item.abstractNumber].join(' ')}</p>
                 </div>
       ))}
         </div>
