8174b7598bec99405b29d840831cd31d91ec1aad
Nicholas Pease <me@nicholaspease.com>
2025-04-01 14:19:35 +0000

    refactor: remove logs, update tests

--- Website/src/components/ListView.jsx
+++ Website/src/components/ListView.jsx
@@ -1,9 +1,6 @@
 import "./ListView.css"
 
-function ListView(props) {
-
-    console.log(props.data) // <-- Here's the data for your ticket
-
+function ListView() {
     return (
         <div id="list-container">
             Welcome to the list view page
--- Website/tests/components/Homepage/Homepage.test.tsx
+++ Website/tests/components/Homepage/Homepage.test.tsx
@@ -5,8 +5,8 @@ import { expect, it } from "vitest";
 import Homepage from '../../../src/components/Homepage';
 
 const mockData = [
-    { title: "Abstract 1", author: "Author A", professor: "Prof. X", school: "School Y", Major_1: "Major Z", category: "Category 1", abstractNumber: "001" },
-    { title: "Abstract 2", author: "Author B", professor: "Prof. Y", school: "School X", Major_1: "Major A", category: "Category 2", abstractNumber: "002" }
+    { title: "Abstract 2", author: "Author B", professor: "Prof. Y", school: "School X", Major_1: "Major A", category: "Category 2", abstractNumber: "002" },
+    { title: "Abstract 1", author: "Author A", professor: "Prof. X", school: "School Y", Major_1: "Major Z", category: "Category 1", abstractNumber: "001" }
 ];
 
 it('Homepage renders correctly and updates abstract', () => {
