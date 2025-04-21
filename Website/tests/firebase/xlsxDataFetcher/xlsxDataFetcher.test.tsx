56c4c9fca17cf22aa5be99cf2c1e42d07a5e9c83
Nicholas Pease <me@nicholaspease.com>
2025-04-21 15:16:09 +0000

    test: update to use previous year instead of current year

--- Website/tests/firebase/xlsxDataFetcher/xlsxDataFetcher.test.tsx
+++ Website/tests/firebase/xlsxDataFetcher/xlsxDataFetcher.test.tsx
@@ -3,12 +3,12 @@ import { expect, it } from "vitest";
 import { fetchXLSXData, parseInput } from '../../../src/firebase/xlsxDataFetcher';
 
 it('Data can be fetched', async () => {
-    await fetchXLSXData('2025')
+    await fetchXLSXData('2024')
     .then(data => expect(data).toBeDefined())
 });
 
 it('Data can be parsed', async () => {
-    await fetchXLSXData('2025')
+    await fetchXLSXData('2024')
     .then(data => parseInput(data['studentApplicationsLinkedRaw']))
     .then(parsedData => expect(parsedData).toBeDefined())
 });
